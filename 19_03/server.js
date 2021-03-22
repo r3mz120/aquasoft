d6d4f9730f1238e924571219745b7ab86deabcbf
Alex <barbudaniel99@gmail.com>
2021-03-22 13:27:59 +0200

    socket.io added

--- 19_03/server.js
+++ 19_03/server.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const router = require("./routes");
 const app = express();
-
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 let port = 8081;
 
 app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log(socket.id + " connected!");
+
+  socket.once("disconnect", () => {
+    console.log(socket.id + " disconnected!");
+  });
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Running...");
 });
 
 app.use("/api", router);
-app.listen(port, () => {
+
+app.io = io;
+
+server.listen(port, () => {
   console.log("Server is runing on " + port);
 });
